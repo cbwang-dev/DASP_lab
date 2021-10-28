c26f7ce266827d5a85f6388736ae8f7f737e40e6
b'Chengbin Wang <r0773655@esat.kuleuven.be>
2021-10-28 22:37:55 +0200

    some bugs in SOE.m, still cannot find it.

--- lab_session_1/SOE.m
+++ lab_session_1/SOE.m
@@ -20,7 +20,10 @@ load HRTF
 siglength = 10;
 
 % Load the speech signal and resample
-source_filename{1} = 'speech1.wav';
+source_filename{1} = 'audio_files/speech1.wav';
+[source,fs] = audioread(source_filename{1}); 
+source = source(1:fs*siglength);
+
 
 % Noise flag for the noise perturbing SOE
 noiseFlag = 0;
@@ -39,23 +42,37 @@ RIR_sources = RIR_sources(1:Lh,:,:);
 
 % Calculate delay for SOE
 Delta=ceil(sqrt(room_dim(1)^2+room_dim(2)^2)*fs_RIR/340);
+% RIR_sources = delayseq(RIR_sources,Delta); % delayed sequence of RIR
 
 % Define the Toeplitz matrices for left and right ear (left side of SOE)
-HL=[0];
-HR=[0];
+HL=[]; 
+for j=1:J
+    first_column=[RIR_sources(:,1,j)' zeros(1,Lg-1)];
+    first_row=[RIR_sources(1,1,j) zeros(1,Lg-1)];
+    part_toeplitz=toeplitz(first_column,first_row);
+    HL = [HL part_toeplitz];
+end
+HR=[];  
+for j=1:J
+    first_column=[RIR_sources(:,2,j)' zeros(1,Lg-1)];
+    first_row=[RIR_sources(1,2,j) zeros(1,Lg-1)];
+    part_toeplitz=toeplitz(first_column,first_row);
+    HR = [HR part_toeplitz];
+end
 
 % Define the HRTFs for left and right ear (right side of SOE) from the
 % loaded HRTF
-% xL = ; % Left ear
-% xR = ; % Right ear
+xL = [1 zeros(1,Lh+Lg-2)]'; % Left ear
+xR = [1 zeros(1,Lh+Lg-2)]'; % Right ear
 
 % Construct H (from HL and HR) and x (from xL and xR) and remove all-zero 
 % rows in H, and the corresponding elements in x
-
+H = [HL;HR];
+x = [xL;xR];
 
 % Solve the SOE
 if ~noiseFlag
-    % Without noise
+    g=H\x;
 else
     % With noise
 end
