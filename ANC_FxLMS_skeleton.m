feeb23f7ff38ea546eb1c71bd7d4f70005c27e5f
matko031 <matkomatic59@gmail.com>
2021-12-08 09:22:27 +0100

    wip

--- ANC_FxLMS_skeleton.m
+++ ANC_FxLMS_skeleton.m
@@ -6,22 +6,26 @@
 % (3) Compute input/output SNRs and SNR improvement.
 % (4) Implement the filter for left and right ears. Hear the synthesised signal
 
-clear all;
-% close all
-
+clearvars;
+close all;
 % Load RIRs
-load ComputedRIRs.mat
+load Computed_RIRs.mat
+M = 400;  % Length of secondary path (RIR)
+L = 400;  % Adaptive filter length
+p = RIR_noise(:,1);
+h = RIR_sources(1:M,1,1);
 % Set length
 sigLenSec = 5; 
 RIRtruncSamples = 500;
 fs_resample = 8000;
+sigLenSample = sigLenSec*fs_resample;
 speech_filename='audio_files/speech1.wav';
 noise_filename='audio_files/White_noise1.wav';
 
 %%
 % Plot the RIRs of the noise
-figure(1); clf;
-plot(RIR_noise(1:400,1));
+% figure(1); clf;
+% plot(RIR_noise(1:400,1));
 
 % Read in the noise source (resample if necessary)
 [noise_raw,fs_noise]=audioread(noise_filename);
@@ -29,52 +33,66 @@ noise_raw=noise_raw(1:fs_noise*sigLenSec);
 noise=resample(noise_raw,fs_resample,fs_noise);
 
 % Plot the noisy signal
-figure(2); clf;
-plot(noise)
+% figure(2); clf;
+% plot(noise)
 
+filt_noise = conv(noise, p);
+filt_noise = filt_noise(1:size(noise,1));
 %% FxLMS
 
-M = 400;  % Length of secondary path (RIR)
-L = 400;  % Adaptive filter length
+
 
 mu = 0.5;   % Step size
+delta = 5*10^-5;
 
-W = zeros(L,1); % Initialize adaptive filter
-    
+w = zeros(L,1); % Initialize adaptive filter
+x = noise;
+d = filt_noise;
+y = zeros(sigLenSample, 1);
+y_h = zeros(sigLenSample, 1);
+e = zeros(sigLenSample, 1);
+xf = zeros(sigLenSample, 1);
 tic
-for n = 1:sigLenSample
+for n = L:sigLenSample
 
-    % STEP 1 : Arrange the previous L + M − 1 samples of x(n) up until the
-    % current sample, n, in an [M x L] Hankel matrix X_Hmat (this will be
-    % use for the filtering operation)
-    X_Hmat = 
+    % STEP 1 : Arrange the previous L−1 samples of x(n) up until the
+    % current sample, n, in an [Lx1] vector
+    len_x_samples = max(M,L);
+    samples_x = x(n-len_x_samples+1:n, 1);
 
     % STEP 2: Filter the signal x(n) with the adaptive filter w(n) to
     % generate loudspeaker signals y(n). Store the samples of y(n) into the
     % vector y
-    y =
+    y(n, 1) = w' * flip(samples_x);
+    samples_y = y(n-M+1:n, 1);
+    y_h(n, 1) = h'*flip(samples_y);
     
     % STEP 3: Compute the error signal e(n)
-    e(n) =
+    e(n, 1) =  filt_noise(n,1) + y_h(n,1);
 
     % STEP 4: Pre-filter x(n) with the (estimated) RIRs to obtain the
     % filtered x(n), \bar{x}(n). Store the samples of \bar{x}(n) into
     % the vector xf
-    xf = 
+    xf(n) = h' * flip(samples_x);
     
     % STEP 5: Update the filter w(n). Store the samples of w(n) into
     % the vector w
-    w = 
+    w = w - (mu / (norm(xf(n,1))^2 + delta)) * xf(n,1)*e(n,1);
 end
 toc
 
 
 %%
 % Calculate the noise suppression
-
+C = 10*log10((e.^2) ./ (d.^2));
 
 %%
 % In the existing plot of the noisy signal, superimpose the error signal to
 % appreciate the noise cancellation
 
-figure(2); hold on;
+figure();
+plot(noise);
+hold on;
+plot(e);
+title("error e")
+hold off;
