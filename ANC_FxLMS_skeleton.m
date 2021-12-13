3abb7a9de747e9221322ec2cc7fd81be2811c40a
matko031 <matkomatic59@gmail.com>
2021-12-13 10:31:21 +0100

    implemented ANC, but it doesnt work fully yet

--- ANC_FxLMS_skeleton.m
+++ ANC_FxLMS_skeleton.m
@@ -14,85 +14,79 @@ M = 400;  % Length of secondary path (RIR)
 L = 400;  % Adaptive filter length
 p = RIR_noise(:,1);
 h = RIR_sources(1:M,1,1);
+
+
 % Set length
-sigLenSec = 5; 
-RIRtruncSamples = 500;
+sigLenSec = 2; 
 fs_resample = 8000;
 sigLenSample = sigLenSec*fs_resample;
 speech_filename='audio_files/speech1.wav';
 noise_filename='audio_files/White_noise1.wav';
+
+% Read in the noise source (resample if necessary)
+[noise_raw,fs_noise]=audioread(noise_filename);
+noise_raw=noise_raw(1:fs_noise*sigLenSec);
+noise=resample(noise_raw,fs_resample,fs_noise);
+d = conv(noise, p);
+d = d(1:size(noise,1));
+
+% soundsc(noise,fs_resample);pause;
+% soundsc(filt_noise,fs_resample);pause;
 
 %%
 % Plot the RIRs of the noise
 % figure(1); clf;
 % plot(RIR_noise(1:400,1));
 
-% Read in the noise source (resample if necessary)
-[noise_raw,fs_noise]=audioread(noise_filename);
-noise_raw=noise_raw(1:fs_noise*sigLenSec);
-noise=resample(noise_raw,fs_resample,fs_noise);
-
 % Plot the noisy signal
 % figure(2); clf;
 % plot(noise)
 
-filt_noise = conv(noise, p);
-filt_noise = filt_noise(1:size(noise,1));
+
 %% FxLMS
-
-
-
-mu = 0.5;   % Step size
+mu = 0.4;
 delta = 5*10^-5;
 
-w = zeros(L,1); % Initialize adaptive filter
 x = noise;
-d = filt_noise;
+xf = conv(x, h);
+
+w = zeros(L, 1);
+e = zeros(sigLenSample,1);
 y = zeros(sigLenSample, 1);
-y_h = zeros(sigLenSample, 1);
-e = zeros(sigLenSample, 1);
-xf = zeros(sigLenSample, 1);
+
 tic
 for n = L:sigLenSample
 
-    % STEP 1 : Arrange the previous L−1 samples of x(n) up until the
-    % current sample, n, in an [Lx1] vector
-    len_x_samples = max(M,L);
-    samples_x = x(n-len_x_samples+1:n, 1);
+    x_samples = x(n:-1:n-L+1); % get last L samples of X in reverse order
+  
+    y(n,1) = w' * x_samples; % get y[n] by multiplying reversed x samples with w
+    y_samples = y(n:-1:n-L+1,1); % get last L samples of y in reverse order
+    y_h = h' * y_samples; % get y_h[n] by multiplying reversed y samples with h
 
-    % STEP 2: Filter the signal x(n) with the adaptive filter w(n) to
-    % generate loudspeaker signals y(n). Store the samples of y(n) into the
-    % vector y
-    y(n, 1) = w' * flip(samples_x);
-    samples_y = y(n-M+1:n, 1);
-    y_h(n, 1) = h'*flip(samples_y);
-    
-    % STEP 3: Compute the error signal e(n)
-    e(n, 1) =  filt_noise(n,1) + y_h(n,1);
+    e(n) = y_h + d(n); % error is the sum of y_h[n] and d[n]
 
-    % STEP 4: Pre-filter x(n) with the (estimated) RIRs to obtain the
-    % filtered x(n), \bar{x}(n). Store the samples of \bar{x}(n) into
-    % the vector xf
-    xf(n) = h' * flip(samples_x);
-    
-    % STEP 5: Update the filter w(n). Store the samples of w(n) into
-    % the vector w
-    w = w - (mu / (norm(xf(n,1))^2 + delta)) * xf(n,1)*e(n,1);
+    xw = xf(n-L+1:n); % get the last L samples of xf (x filtered with h)
+    w = w + ( mu / ((norm(xw))^2 + delta) ) * xw*e(n); % calculate new w
+
 end
 toc
 
 
 %%
 % Calculate the noise suppression
-C = 10*log10((e.^2) ./ (d.^2));
+E_e = mean( e(round(0.1*sigLenSample):end,1) ); % approximate E(e), by taking the mean of 90% last values of e
+E_d = mean( d(round(0.1*sigLenSample):end,1) ); % approximate E(d), by taking the mean of 90% last values of d
+
+C = 10*log10((E_e.^2) ./ (E_d.^2));
 
 %%
 % In the existing plot of the noisy signal, superimpose the error signal to
 % appreciate the noise cancellation
-
-figure();
-plot(noise);
+% 
+figure(1);
+plot(d, 'DisplayName', "noise");
 hold on;
-plot(e);
-title("error e")
+plot(e, 'DisplayName', "error");
 hold off;
+legend;
+title('Comparison of noise and error')
--- ANC_MFxLMS_skeleton.m
+++ ANC_MFxLMS_skeleton.m
@@ -14,37 +14,40 @@ close all;
 load Computed_RIRs.mat
 M = 400;  % Length of secondary path (RIR)
 L = 400;  % Adaptive filter length
-p = RIR_noise(:,:);
-h = RIR_sources(1:M,:,:);
+J = size(RIR_sources,3);
+p1 = RIR_noise(:,1);
+p2 = RIR_noise(:,2);
+h1 = RIR_sources(1:L, 1, :);
+h1 = reshape(h1,L, J);
+h2 = RIR_sources(1:L, 2, :);
+h2 = reshape(h2,L, J);
+
+
 % Set length
-sigLenSec = 5; 
-RIRtruncSamples = 500;
+sigLenSec = 2; 
 fs_resample = 8000;
 sigLenSample = sigLenSec*fs_resample;
 speech_filename='audio_files/speech1.wav';
 noise_filename='audio_files/White_noise1.wav';
 
-%%
-% Plot the RIRs of the noise
-% figure(1); clf;
-% plot(RIR_noise(1:400,1));
-
 % Read in the noise source (resample if necessary)
 [noise_raw,fs_noise]=audioread(noise_filename);
 noise_raw=noise_raw(1:fs_noise*sigLenSec);
 noise=resample(noise_raw,fs_resample,fs_noise);
+d1 = conv(noise, p1);
+d1 = d1(1:size(noise,1));
+d2 = conv(noise, p2);
+d2 = d2(1:size(noise,1));
+d = [d1 d2];
 
-% Plot the noisy signal
-% figure(2); clf;
-% plot(noise)
-
-filt_noise = conv2(noise, p);
+% soundsc(noise,fs_resample);pause;
+% soundsc(filt_noise,fs_resample);pause;
 
 
 % Run the cross cancel from session 1 to obtain the auralized speech
 
 %% Plot the RIRs of the noise
-figure(1); clf; 
+% figure(1); clf; 
 
 %%
 
@@ -54,64 +57,89 @@ figure(1); clf;
 % addBinSig = 1;
 
 % Plot the noisy signals (1 for each subplot)
-figure(2); clf;
+% figure(2); clf;
 
 
 %% MFxLMS
 
-M = 400;  % Length of secondary path (RIR)
-L = 400;  % Adaptive filter length
-J = 5;
+
 mu = 0.5;   % Step size
-y_h = zeros(sigLenSample, 2, J);
-e = zeros(sigLenSample, 2);
-xf = zeros(sigLenSample, 2, J);
-w = zeros(L,J); % Initialize adaptive filter
-y = zeros(L, J);
+delta = 10^-5; % regularization factor
+
+y = zeros(L, J); % vector of x samples filetered by w
+y_h = zeros(sigLenSample, 2, J); % vector of y samples filtered by h
+
+e1 = zeros(sigLenSample, 1); % channel1 error
+e2 = zeros(sigLenSample, 1); % channel2 error
+e = zeros(sigLenSample, 2); % dual channel error
+
 x = noise;
+xf1 = zeros(sigLenSample, J); % xbar of channel 1
+xf2 = zeros(sigLenSample, J); % xbar of channel 2
+
+for i=1:J % convolve xf1 and xf2 with h
+    xf_tmp = conv(x, h1(:,i));
+    xf1(:,i) = xf_tmp(1:sigLenSample,1);
+    
+    xf_tmp = conv(x, h2(:,i));
+    xf2(:,i) = xf_tmp(1:sigLenSample,1);
+end
+
+w = zeros(L,J); % Initialize adaptive filter
 tic
+
+%%
 for n=L:sigLenSample
    
-    % STEP 1 : Arrange the previous L−1 samples of x(n) up until the
-    % current sample, n, in an [Lx1] vector
-    samples_x = x(n-L+1:n, :);
+   
+    samples_x = x(n:-1:n-L+1, 1);
 
-    % STEP 2: Filter the signal x(n) with the adaptive filter w(n) to
-    % generate loudspeaker signals y(n). Store the samples of y(n) into the
-    % vector y
-    y(n, :) = flip(samples_x)' * w;
-    samples_y = y(n-M+1:n, :);
-    y_h(n, 1, :) = flip(samples_y)' * reshape(h(:, 1, :), M, J); 
-    y_h(n, 2, :) = flip(samples_y)' * reshape(h(:, 2, :), M, J); 
+    y(n, :) = w' * samples_x;
+    samples_y = y(n:-1:n-M+1, :);
 
-    % STEP 3: Compute the error signal e(n)
-    e(n, :) =  filt_noise(n,:) + sum(y_h(n,:,:) ,3);
+    y_h1 = sum(h1 .* samples_y); 
+    % multiply reversed y samples from each channel with respective h coefficients element wise 
+    % and sum them column wise in order to receive vector y_h1
 
-    % STEP 4: Pre-filter x(n) with the (estimated) RIRs to obtain the
-    % filtered x(n), \bar{x}(n). Store the samples of \bar{x}(n) into
-    % the vector xf
-    xf(n, 1, :) = flip(samples_x)' * h(:, 1, :);
-    xf(n, 2, :) = flip(samples_x)' * h(:, 2, :);
+    y_h2 = sum(h2 .* samples_y); 
+    % analogous to y_h1
 
-    % STEP 5: Update the filter w(n). Store the samples of w(n) into
-    % the vector w
+    e1(n,1) = d1(n,1) + sum(y_h1, 'all'); % sum all y_h1 values and add them to e1 to get the error
+    e2(n,1) = d2(n,1) + sum(y_h2, 'all'); % same as e1
+    e(n, :) = [e1(n,1) e2(n,1)];
+   
     for j = 1:J
-        w(:, j) = w(:, j) - (mu / (norm(xf(n,:,j), 'fro')^2 + delta)) * xf(n,:, j)*e(n,:)';
+        Xj = [xf1(n-L+1:n,1) xf2(n-L+1:n,1)]; % construct Xj matrix from L last samples of xf1 and xf2
+        w(:, j) = w(:, j) - (mu / (norm(Xj, 'fro')^2 + delta)) * Xj*e(n,:)'; % update w
     end
 end
 toc
 
 %%
 % Calculate the noise suppression
+E_e = mean( e(round(0.1*sigLenSample):end,:) ); % approximate E(e), by taking the mean of 90% last values of e
+E_d = mean( d(round(0.1*sigLenSample):end,:) ); % approximate E(d), by taking the mean of 90% last values of d
+
+C1 = 10*log10((E_e(:,1).^2) ./ (E_d(:,1).^2));
+C2 = 10*log10((E_e(:,2).^2) ./ (E_d(:,2).^2));
 
 %%
 % In the existing plot of the noisy signals, superimpose the corresponding
 % error signals to appreciate the noise cancellation
 
-figure();
-plot(noise);
+figure(2);
+
+subplot(2,1,1)
+plot(d(:,1), 'DisplayName', "noise");
 hold on;
-plot(e(:,1));
-plot(e(:,2));
-title("error e")
+plot(e(:,1), 'DisplayName', "error");
+title("d-e channel 1")
+hold off;
+legend;
+
+subplot(2,1,2)
+plot(d(:,2), 'DisplayName', "noise");
+hold on;
+plot(e(:,2), 'DisplayName', "error");
+title("d-e channel 2")
 hold off;
